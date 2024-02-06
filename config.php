6e91c3ba13500fd69bef5c44a4ee8eb00c7a60d4
@CaiqueBraga <caiquebsousa@outlook.com>
2024-02-06 15:58:56 -0300

    [REFS: 0002 - Feat - Recuperando os dados do formulário e salvando no banco de dados]

--- config.php
+++ config.php
@@ -17,3 +17,32 @@ try {
     //caso ocorra algum erro durante conexão, eiba uma msg de erro
     echo "Erro de conexão: " . $e->getMessage();
 }
+
+
+//Recuperando dados do formulário
+
+$nome = $_POST["nome"];
+$email = $_POST["email"];
+$senha = password_hash($_POST["senha"], PASSWORD_DEFAULT); //Hash da senha usando password_hash
+
+try{
+
+    //Prepara a instrução SQL para inserir no DB
+    $stm = $coon->prepare("INSERT INTO usuarios (nome, email, senha) VALUES (:nome, :email, :senha)");
+
+    //Substitu os parâmetros da instrução do SQL pelos valores fornecidos
+    $stm->bindParam(':nome', $nome);
+    $stm->bindParam(':email', $email);
+    $stm->bindParam(':senha', $senha);
+
+    //executa a instrução preparada
+
+        echo "Novo registro inserido com sucesso.";
+} catch (PDOException $e){
+    //Caso ocorra algum erro durante a execução da instrução SQL, exibe a msg
+    echo "Erro ao inserir registro: " . $e->getMessage();
+}
+
+// Por fim fechamos a conexão
+$coon = null;
+
